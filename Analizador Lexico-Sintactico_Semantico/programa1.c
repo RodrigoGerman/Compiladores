89709ed3416010f9d525eb50cd96c94e75f0047e
Rodrigo German Lopez <33498070+RodrigoGerman@users.noreply.github.com>
2020-06-23 14:01:54 -0500

    Add files via upload

---
+++ Analizador Lexico-Sintactico_Semantico/programa1.c
@@ -0,0 +1,14 @@
+[
+	Entero ax,ay,zx,sfd;
+	Real bx,xz,gf,mm;
+	Bul bdg,dgs,jd,xx,xxx,yyy,xyz;
+	Cadena gdt,lfdj,ldodf;
+	bx := 5.15E-5;
+	ax := 10E-5;
+	Si (bx .MN. ax)[
+		ay := bx * ax;
+	]Sino[
+		ay := 5E-5;
+	]
+
+]
---
+++ Analizador Lexico-Sintactico_Semantico/programa2.c
@@ -0,0 +1,9 @@
+[
+	Bul uno, dos;
+	Entero cont,var ;
+	var := 5+4
+	rav := dos;
+	Si(var .MNI. rav)[Para(cont := 0; cont .MN. dos; cont += 1) [var += 3;]]
+]
+
+
